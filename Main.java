be9ce23b2f51b497a7e4f20ae6e2f62d8b50c770
zozo1011 <zenabsabra2016@gmail.com>
2026-01-16 12:15:22 +0200

    Initial commit - Java cab project

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+      System.out.println("Welcome to Online Cab Service");  
+      System.out.println("Booking system initialized...");
+    }
+}
